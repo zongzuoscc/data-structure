0dee8d0c78a6f0949ff6939932fed4f7ac1f68ab
YangYiming <2651575456@qq.com>
2024-04-06 19:07:05 +0800

    添加栈，队列具体实现代码

--- 栈.cpp
+++ 栈.cpp
@@ -1,13 +1,13 @@
 //先进后出
 //此处为自己用数组实现栈的操作，并不是用stl中的stack
-#include <iostream>
+//#include <iostream>
 
-using namespace std;
+//using namespace std;
 
-const int N=100010;
+// const int N=100010;
 //tt表示栈点下标，stk表示栈
-int stk[N],tt;
-int x;
+// int stk[N],tt;
+// int x;
 //插入
 //stk[++tt]=x;
 //弹出
@@ -19,3 +19,39 @@ int x;
 
 //栈顶
 //stk[tt];
+
+//具体的实现过程
+#include <iostream>
+#include <string>
+using namespace std;
+const int N = 100010;
+int m;
+int stk[N], tt;
+
+int main()
+{
+    cin >> m;
+    while (m -- )
+    {
+        string op;
+        int x;
+
+        cin >> op;
+        if (op == "push")
+        {
+            cin >> x;
+            stk[ ++ tt] = x;
+        }
+        else if (op == "pop") 
+        {
+            if(tt>0)
+            {
+                tt--;
+            }
+        }
+        else if (op == "empty") cout << (tt ? "NO" : "YES") << endl;
+        else cout << stk[tt] << endl;
+    }
+
+    return 0;
+}
--- 队列.cpp
+++ 队列.cpp
@@ -1,10 +1,10 @@
 //先进先出
-#include <iostream>
-using namespace std;
-const int N=100010;
+// #include <iostream>
+// using namespace std;
+// const int N=100010;
 //hh是队头，tt是队尾
 //在队尾插入元素，在队头弹出元素
-int q[N],hh,tt=-1;
+// int q[N],hh,tt=-1;
 //插入
 //q[++tt]=x;
 
@@ -20,3 +20,32 @@ int q[N],hh,tt=-1;
 
 //取出队尾元素
 //q[tt];
+#include <iostream>
+#include <string>
+using namespace std;
+const int N=100010;
+int m;
+int q[N], hh, tt = -1;
+
+int main()
+{
+    cin >> m;
+
+    while (m -- )
+    {
+        string op;
+        int x;
+
+        cin >> op;
+        if (op == "push")
+        {
+            cin >> x;
+            q[ ++ tt] = x;
+        }
+        else if (op == "pop") hh ++ ;
+        else if (op == "empty") cout << (hh <= tt ? "NO" : "YES") << endl;
+        else cout << q[hh] << endl;
+    }
+
+    return 0;
+}
