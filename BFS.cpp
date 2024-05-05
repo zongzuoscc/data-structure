0e477994eda018191e099c5e4506c2075ed966f7
YangYiming <2651575456@qq.com>
2024-05-05 19:17:14 +0800

    增加BFS的另一种写法

--- BFS.cpp
+++ BFS.cpp
@@ -67,3 +67,55 @@ int main()
     cout<<bfs()<<endl;
     return 0;
 }
+
+/*  以下为使用stl中的队列进行编写的代码
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <queue>
+using namespace std;
+const int N = 110;
+int n, m;
+int g[N][N], d[N][N];
+int bfs()
+{
+    queue<pair<int,int>> q;
+
+    memset(d, -1, sizeof d);
+    d[0][0] = 0;//表示已经走过了    
+    q.push({0, 0});
+
+    int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};  //这里是向量表示
+
+    while (q.size())  //队列不空
+    {
+        auto t = q.front();//   这里t的类型就是 pair<int,int>   每一次取出队头元素
+        q.pop();
+
+        for (int i = 0; i < 4; i ++ )  //每次枚举四个方向
+        {
+            int x = t.first + dx[i], y = t.second + dy[i];
+
+            if (x >= 0 && x < n && y >= 0 && y < m && g[x][y] == 0 && d[x][y] == -1)
+            {
+                d[x][y] = d[t.first][t.second] + 1;
+                q.push({x, y});
+            }
+        }
+    }
+
+    return d[n - 1][m - 1];
+}
+
+int main()
+{
+    cin >> n >> m;
+    for (int i = 0; i < n; i ++ )
+        for (int j = 0; j < m; j ++ )
+            cin >> g[i][j];
+
+    cout << bfs() << endl;
+
+    return 0;
+}
+*/
