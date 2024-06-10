c9277fdc4b6963d7268fd8dd2cb19e9c3b80c506
YangYiming <2651575456@qq.com>
2024-06-10 22:27:03 +0800

    提交扩展欧几里得算法

--- test.cpp
+++ test.cpp
@@ -1,40 +1,33 @@
-//快速幂可以快速地求出  a^k mod b 的结果  时间复杂度是o(log k)
-//核心思路 反复平方法
-//预处理出来
-// 给定 n组 ai,bi,pi，对于每组数据，求出 ai^bi mod pi的值。
 #include <iostream>
 #include <algorithm>
 using namespace std;
-
-//a^k%p
-int qmi(int a,int k,int p)
+const int N=100010;
+int q[N];
+void quicksort(int q[],int l,int r)
 {
-    int res=1;//a^k mod p
-    while(k)
+    if(l>=r) return;
+    int i=l-1,j=r+1,x=q[l+r>>1];
+    while(i<j)
     {
-        if(k&1)//如果当前k的末尾是1
-        {
-            res=(long long)res*a%p;
-        }
-        //a是不断反复平方的
-        k>>=1;//把k的末位删掉，因为已经处理完了  右移一位，相当于将 k 除以 2 并去掉余数部分。
-        a=(long long)a*a%p;//更新a为它的平方，并取模 p。这个步骤的作用是将 a 不断平方，以便处理更高位的 k。
+        do i++; while(q[i]<x);
+        do j--; while(q[j]>x);
+        if(i<j) swap(q[i],q[j]);
     }
-    return res;
+    quicksort(q,l,j);
+    quicksort(q,j+1,r);
 }
-
 int main()
 {
     int n;
     cin>>n;
-    while(n--)
+    for(int i=0;i<n;i++)
     {
-        int a;//底数 即需要计算的底数
-        int k;//质数 即幂的指数
-        int p;//模数 用于取模运算
-        cin>>a>>k>>p;
-
-        cout<<qmi(a,k,p)<<endl;
+        cin>>q[i];
+    }
+    quicksort(q,0,n-1);
+    for(int i=0;i<n;i++)
+    {
+        cout<<q[i]<<" ";
     }
     return 0;
 }
---
+++ 扩展欧几里得算法.cpp
@@ -0,0 +1,45 @@
+//之前讲过欧几里得算法 就是辗转相除法
+//给定 n对正整数 ai,bi，对于每对数，求出一组 xi,yi，使其满足 ai*xi+bi*yi=gcd(ai,bi)。
+//裴蜀定理
+//  对于任意的 一对正整数a,b 那么存在非零整数x,y  使得ax+by=(a,b) （即a和b的最大公约数）
+//而且这个最大公约数 是a和b能凑出来最小的正整数
+//那么这是为什么呢 
+//      假设存在ax+by=d  那么d一定是a,b最大公约数的倍数
+
+//这个求出来的解只是其中之一  以下为通解公式 即只要求出来一个 就可以把剩下的全部求出来
+//x=x0-(b/d)  *k k是整数
+//y=y0+(b/d)  *k
+
+#include <iostream>
+using namespace std;
+
+int exgcd(int a,int b,int &x,int &y)
+{
+    //return b?gcd(b,a%b):a; 这是简写 ，我们在这里要把它详细展开
+    if(!b)
+    {
+        x=1,y=0;//当b等于0时
+        return a;
+    }
+    //当我们的递归结束的时候，就已经会得到一个y和x使得  by+(a%b)*x=(a,b)
+    //整理之后a mod b = a-(a/b)(向下取整)*b
+    //再整理得ax + b(y-(a/b)(向下取整)*x)==d
+    int d = exgcd(b,a%b,y,x);//x和y反转的原因是因为前面也翻转了  这样更有利于计算
+    //x不用变
+    y-=a/b*x;
+    return d;
+}
+
+int main()
+{
+    int  n;
+    cin>>n;
+    while(n--)
+    {
+        int a,b,x,y;
+        cin>>a>>b;
+        exgcd(a,b,x,y);
+        cout<<x<<" "<<y<<endl;
+    }
+    return 0;
+}
